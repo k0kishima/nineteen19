490ad3ffc5b848d56ee53aed9db406ad0febdfd1
Kohta Okishima@MBA <kohta.okishima@vertex-japan.jp>
2013-11-28 20:35:51 +0900

    Initial Commit

---
+++ nineteen19/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  nineteen19
+//
+//  Created by Kohta Okishima on 2013/11/28.
+//  Copyright (c) 2013年 VERTEX-JAPAN. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ nineteen19/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  nineteen19
+//
+//  Created by Kohta Okishima on 2013/11/28.
+//  Copyright (c) 2013年 VERTEX-JAPAN. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
