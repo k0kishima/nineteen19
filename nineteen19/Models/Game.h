3d899c169e2734173f6873ca29708da75612b66a
Kohta Okishima@MBA <kohta.okishima@vertex-japan.jp>
2013-12-02 22:41:44 +0900

    Add Game model
    And implemented prepareQuestions method in the Game model.

---
+++ nineteen19/Models/Game.h
@@ -0,0 +1,17 @@
+//
+//  Game.h
+//  nineteen19
+//
+//  Created by Kohta Okishima on 2013/12/02.
+//  Copyright (c) 2013年 VERTEX-JAPAN. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Game : NSObject
+@property NSMutableArray *questions;
+@property NSMutableArray *answers;
+
+
+- (void) prepareQuestions;
+@end
---
+++ nineteen19Tests/GameSpec.m
@@ -0,0 +1,68 @@
+//
+//  GameSpec.m
+//  nineteen19
+//
+//  Created by Kohta Okishima on 2013/12/02.
+//  Copyright (c) 2013年 VERTEX-JAPAN. All rights reserved.
+//
+
+#import "Game.h"
+#import "Kiwi.h"
+
+SPEC_BEGIN(GameSpec)
+
+describe(@"Game", ^{
+    __block Game *game;
+    
+    beforeAll(^{
+        game = [Game alloc];
+    });
+    
+    describe(@"-prepareQuestions", ^{
+        beforeAll(^{
+            [game prepareQuestions];
+        });
+        
+        describe(@"initializes the questions property and the answers property", ^{
+            describe(@".questions", ^{
+                describe(@"a element", ^{
+                    it(@"'s key is the answer and value is the expression of the answer", ^{
+                        int x = 11;
+                        int y = 12;
+                        int answer = x * y;
+                        
+                        NSMutableArray *expressions = [game.questions objectAtIndex:answer];
+                        NSMutableArray *operands = [expressions objectAtIndex:0];
+                        
+                        [[theValue(x) should] equal:theValue([[operands objectAtIndex:0] intValue])];
+                        [[theValue(y) should] equal:theValue([[operands objectAtIndex:1] intValue])];
+                        [[theValue(answer) should] equal:theValue([[operands objectAtIndex:0] intValue] * [[operands objectAtIndex:1] intValue])];
+                    });
+                });
+
+            });
+            
+            describe(@".answers", ^{
+                it(@"is from 11 * 11 to 19 * 19", ^{
+                    int answerOfTheFirstQuestion = 11 * 11;
+                    int answerOfTheLastQuestion  = 19 * 19;
+                    
+                    [[theValue(answerOfTheFirstQuestion) should] equal:theValue([[game.answers firstObject] intValue])];
+                    [[theValue(answerOfTheLastQuestion)  should] equal:theValue([[game.answers lastObject] intValue])];
+                });
+                it(@"has excluded easy questions", ^{
+                    int x = 11 + arc4random() % 9;
+                    int y = 10;
+                    NSNumber *answerOfTheEasyQuestion = [NSNumber numberWithInt:(x * y)];
+                    
+                    NSInteger anIndex = [game.answers indexOfObject:answerOfTheEasyQuestion];
+                    [[theValue(anIndex) should] equal:theValue(NSNotFound)];
+                });
+            });
+        });
+        
+    });
+    
+});
+
+SPEC_END
